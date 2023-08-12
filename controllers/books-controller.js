b09bd3ae212843487af08e7e69bcb10ce8af0abc
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2023-08-12 13:24:02 -0700

    need to finish

--- controllers/books-controller.js
+++ controllers/books-controller.js
@@ -3,15 +3,23 @@ const books = express.Router()
 const Book = require('../models/books')
 
 module.exports = books
-
+//seeded data
+router.get('/data/seed' , (req, res)=>{
+    Book.insertMany(seedData)
+   .then(createdBooks => {
+    res.sendstatus(200);
+   })
+.catch(e =>{
+    console.error(e);
+});
+})
 
 //index 
 route.get('/', async(req, res)=>{
     try{
-        const founBooks: await Book.find();
+        const foundBooks = await Book.find();
+        res.send(foundBooks);
+    } catch(e){
+console.error(e);
     }
-    catch(e){
-
-    }
-}
-)
+})
